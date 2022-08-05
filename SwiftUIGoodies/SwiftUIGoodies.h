7d9af9d6dc1da49411db8fff623f4a944f738374
Ahmed Mgua <mguaahmed@gmail.com>
2022-08-05 21:45:42 +0300

    Initial Commit

---
+++ SwiftUIGoodies/SwiftUIGoodies.h
@@ -0,0 +1,18 @@
+//
+//  SwiftUIGoodies.h
+//  SwiftUIGoodies
+//
+//  Created by Ahmed Mgua on 05/08/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftUIGoodies.
+FOUNDATION_EXPORT double SwiftUIGoodiesVersionNumber;
+
+//! Project version string for SwiftUIGoodies.
+FOUNDATION_EXPORT const unsigned char SwiftUIGoodiesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftUIGoodies/PublicHeader.h>
+
+
