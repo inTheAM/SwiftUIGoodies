2a2d434af50b5b5aa31d476b02c273d5aa7fb530
Ahmed Mgua <68146662+inTheAM@users.noreply.github.com>
2023-05-25 12:03:56 +0200

    Modified rounded border to be only for textfields and added customisation

---
+++ Sources/SwiftUIGoodies/TextField+RoundedBorder.swift
@@ -0,0 +1,39 @@
+//
+//  TextField+RoundedBorder.swift
+//  
+//
+//  Created by Ahmed Mgua on 24/03/23.
+//
+
+import SwiftUI
+
+extension TextField {
+
+    public enum ShapeType {
+        case stroke(lineWidth: CGFloat),
+             fill(opacity: CGFloat)
+    }
+
+    public func roundedBorder(systemImage: String? = nil, cornerRadius: Double, color: Color, style: ShapeType) -> some View {
+        HStack {
+            if let systemImage {
+                Image(systemName: systemImage)
+                    .foregroundColor(.secondary)
+            }
+            self
+        }
+        .padding(.asymmetrical)
+        .background {
+            switch style {
+            case .stroke(let lineWidth):
+                RoundedRectangle(cornerRadius: cornerRadius)
+                    .stroke(lineWidth: lineWidth)
+                    .foregroundColor(color)
+            case .fill(let opacity):
+                RoundedRectangle(cornerRadius: cornerRadius)
+                    .foregroundColor(color.opacity(opacity))
+            }
+
+        }
+    }
+}
